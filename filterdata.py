39aa2eedd9385ce526ff57c6ea920c0a446766e6
Óscar Martínez <masquesig@gmail.com>
2018-04-25 00:56:48 +0200

    Fields ui  bug and new tab duplicates

--- filterdata.py
+++ filterdata.py
@@ -9,66 +9,60 @@ from java.lang import Number
 from javax.swing.table import DefaultTableModel
 from java.awt import BorderLayout
 from org.gvsig.app.gui.filter import FilterDialog
-
 from org.gvsig.fmap.dal.swing import DALSwingLocator
+from javax.swing import JPanel
 
-class AdvancedFilterDialog(FilterDialog):
-    pass
     
-class Panel(FormPanel):
-    view = None
-    
+class FilterLayerPanel(FormPanel):
     def __init__(self):
         FormPanel.__init__(self, os.path.join(os.path.dirname(__file__), "filterdata.xml"))
-        self.view = gvsig.currentView()
+        
+        #self.setPreferredSize(300,300)
         self.nw = None
-
-        # Advanced dialog
-        #tbpanel = AdvancedFilterDialog()
-
-        ##
-        #nw = DALSwingLocator.getSwingManager().createExpression()
-        #self.jpFilter.add(tbpanel)
-        self.jpFilter.updateUI()
-
+        self.view = gvsig.currentView()
+        self.layer = self.view.getLayer(self.cmbLayers.getSelectedItem()).getFeatureStore()
         # Fill dialog
-        layers = gvsig.currentView().getLayers()
+        layers = self.view.getLayers()
         self.cmbLayers.removeAllItems()
         for layer in layers:
-           self.cmbLayers.addItem(str(layer.getName()))
-        self._updateFields()
-        
-        self.nw = DALSwingLocator.getSwingManager().createQueryFilterExpresion(gvsig.currentView().getLayer(self.cmbLayers.getSelectedItem()).getFeatureStore())
-        bl = BorderLayout()
-        self.jpFilter.setLayout(bl)
-        self.jpFilter.add(self.nw)
-        
+           self.cmbLayers.addItem(layer.getName())
+           
+        self.valuesList = self.lstValues
+        self.nw = None
+        if self.layer!=None:
+            store  = self.layer.getFeatureStore()
+            if store != None: 
+                self.nw = DALSwingLocator.getSwingManager().createQueryFilterExpresion(store)
+                bl = BorderLayout()
+                self.pnlFilter.setLayout(bl)
+                self.pnlFilter.add(self.nw)
+
     def _updateAdvancedFilter(self):
-        #self.jpFilter.getComponents()[0]
         if self.nw == None:
             return
-        store = gvsig.currentView().getLayer(self.cmbLayers.getSelectedItem()).getFeatureStore()
-        self.nw.setFeatureStore(store)
+        self.nw.setFeatureStore(self.layer.getFeatureStore())
         
     def _updateFields(self):
         self.cmbFields.removeAllItems()
-        layer = gvsig.currentView().getLayer(self.cmbLayers.getSelectedItem())
-        if layer == None:
+        self.cmbDuplicates.removeAllItems()
+        if self.layer == None:
             return
-        ft = layer.getFeatureStore().getDefaultFeatureType()
+        ft = self.layer.getFeatureStore().getDefaultFeatureType()
         ds = ft.getAttributeDescriptors()
         self.cmbFields.addItem("")
+        self.cmbDuplicates.addItem("")
         for d in ds:
             self.cmbFields.addItem(str(d.getName())) 
+            self.cmbDuplicates.addItem(str(d.getName()))
 
-    def _updateListValues(self):
-        field = self.cmbFields.getSelectedItem()
+    def _getCountValues(self,comboField):
+        #self.lstValues.getModel().removeAllElements()
+        field = comboField.getSelectedItem()
         if field == None or field == "":
             return
-        layer = gvsig.currentView().getLayer(self.cmbLayers.getSelectedItem())
-        if layer == None:
+        if self.layer == None:
             return
-        features = layer.getFeatureStore().getFeatureSet()
+        features = self.layer.getFeatureStore().getFeatureSet()
         count = {}
         for f in features:
             try:
@@ -79,56 +73,87 @@ class Panel(FormPanel):
                 count[ff] += 1
             else:
                 count[ff] = 1
-        #print "COUNT FOR ", field, " : ", count
-        # jlist
+        return count
         
+    def _updateListValues(self):
+        # LIST
+        count = self._getCountValues(self.cmbFields)
+        if count == None: return
         model = DefaultListModel()
         for i in sorted(count.keys()):
             model.addElement(i)
-        self.lstValues.setModel(model)
-        """
-        # jtable
-        dtm = DefaultTableModel()  #ount.keys(), count.values())
-        for i in sorted(count.keys()):
-            dtm.addRow([i, count[i]])
-        self.tbmValues.setModel(tm)
-        dtm.fireTableDataChanged()
-        """
-    
+        self.valuesList.setModel(model)
+
+        
+    def _updateListDuplicates(self):
+        # DUPLICATES TABLE
+        count = self._getCountValues(self.cmbDuplicates)
+        if count == None: return
+        dict_list=[]
+        for i,j in count.iteritems():
+            dict_list.append ((i,j))
+        sorted_list = sorted(dict_list,key=lambda x: x[1],reverse=True)
+        tableModel = DefaultTableModel(sorted_list, ["Field", "Duplicates"])
+        self.tblFilter.setModel(tableModel)
+
     def cmbLayers_change(self, *args):
+        self.layer = gvsig.currentView().getLayer(self.cmbLayers.getSelectedItem())
         self._updateFields()
         self._updateAdvancedFilter()
+        #self._updateListValues()
+        
     def cmbFields_change(self, *args):
         self._updateListValues()
+        
     def lstValues_change(self, *args):
-        self.process()
+        self.process("basicFilter")
+        
+    def btnAdvancedFilter_click(self,*args):
+        self.process("advancedFilter")
+        
+    def cmbDuplicates_change(self,*args):
+        self._updateListDuplicates()
+        
+    def process(self, filterType):#btnProcess_click(self, *args):
+        store = self.layer.getFeatureStore()
 
-    def process(self):#btnProcess_click(self, *args):
-        layer = gvsig.currentView().getLayer(self.cmbLayers.getSelectedItem())
-        store = layer.getFeatureStore()
-        field = self.cmbFields.getSelectedItem()
-        if field == "":
-            # TODO: si el campo esta vacio devolver el query sin filtro
-            #filter = DALLocator.getDataManager().createExpresion()
-            return
-        else:
-            value = self.lstValues.getSelectedValue()
-            store = layer.getFeatureStore()
-            if isinstance(value, Number):
-                expression = '%s = %s'%(field, value)
+        # TODO: si el campo esta vacio devolver el query sin filtro
+        #filter = DALLocator.getDataManager().createExpresion()
+
+        if filterType=="basicFilter":
+            fq = store.createFeatureQuery()
+            fq.retrievesAllAttributes()
+            for field  in store.getDefaultFeatureType().getAttrNames():
+                fq.addAttributeName(field)
+            for value in self.valuesList.getSelectedValuesList():
+                field = self.cmbFields.getSelectedItem()
+                if field == "": return
+                if isinstance(value, Number):
+                    expression = '%s = %s'%(field, value)
+                else:
+                    expression = "%s = '%s'"%(field, value)
+                expression = DALLocator.getDataManager().createExpresion(expression)
+                fq.addFilter(expression)
+            print fq.getFilter()
+            from org.gvsig.tools.evaluator import Evaluator
+            if isinstance(fq, Evaluator):
+                self.layer.addBaseFilter(fq)
             else:
-                expression = "%s = '%s'"%(field, value)
-            filter = DALLocator.getDataManager().createExpresion(expression)
-        fq = store.createFeatureQuery()
-        fq.retrievesAllAttributes()
-        fq.addAttributeName(field)
-        fq.setFilter(filter)
-        layer.setBaseQuery(fq)
+                self.layer.setBaseQuery(fq)
+        elif filterType=="advancedFilter":
+            expression = self.nw.getExpresion()
+            fq = store.createFeatureQuery()
+            fq.retrievesAllAttributes()
+            for field  in store.getDefaultFeatureType().getAttrNames():
+                fq.addAttributeName(field)
+            fq.setFilter(expression)
+            self.layer.setBaseQuery(fq)
+
+        else:
+            return
+
         #port = gvsig.currentView().getMapContext().getViewPort()
         #port.refreshExtent()
-        view = gvsig.currentView()
-        #import pdb
-        #pdb.set_trace()
         #gvsig.currentView().getMapContext().invalidate()
         gsv = self.view.getMapContext().getScaleView()
         self.view.getMapContext().setScaleView(gsv+1)
@@ -139,6 +164,6 @@ class Panel(FormPanel):
         #gvsig.currentView().getMapContext().getViewPort().refreshExtent()
         
 def main(*args):
-    l = Panel()
-    l.showTool("Entities filter")
+    l = FilterLayerPanel()
+    l.showTool("Filter Layer")
     pass
